f7436bd0def8f96f5df89388c5730777e1bee6e4
Manoj Silag <silagmanoj@yahoo.in>
2021-03-28 20:23:35 +0530

    Update App.js

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -4,6 +4,7 @@ import { StylesProvider } from "@material-ui/core/styles";
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
+
 export default function App() {
   return (
     <div>
